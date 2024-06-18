ca7bf60a4c23f836c8bb895af0d0f4aaddb49337
11111Divyam11111 <126981502+11111Divyam11111@users.noreply.github.com>
2024-06-18 20:04:18 +0530

    routes

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -10,32 +10,34 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
-  async session({ session }) {
-    const sessionUser = await User.findOne({ email: session.user.email });
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({ email: session.user.email });
 
-    session.user.id = sessionUser._id.toString();
+      session.user.id = sessionUser._id.toString();
 
-    return session();
-  },
-  async signIn({ profile }) {
-    try {
-      await connectToDB();
+      return session();
+    },
+    async signIn({ profile }) {
+      try {
+        await connectToDB();
 
-      // check if use already exists
-      const userExist = await User.findOne({ email: profile.email });
+        // check if use already exists
+        const userExist = await User.findOne({ email: profile.email });
 
-      // create user and save to DB
-      if (!userExist) {
-        await User.create({
-          email: profile.email,
-          username: profile.name.replace(" ", "").toLowerCase(),
-          image: profile.picture,
-        });
+        // create user and save to DB
+        if (!userExist) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
+        }
+        return true;
+      } catch (error) {
+        return false;
       }
-      return true;
-    } catch (error) {
-      return false;
-    }
+    },
   },
 });
 
